e40d54c9450439a119e376223878806f47324532
Learning-Pythong <lamostlaaa@gmail.com>
2024-07-10 14:05:58 +1000

    Coding Challenge - 1

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let country = "Australia";
-let continent = "Oeania";
+const markMassd1 = 78;
+const johnMassd1 = 92;
 
-let population = "26 Million";
+const markHeightd1 = 1.69;
+const johnHeightd1 = 1.95;
+
+const bmiMarkd1 = markMassd1 / markHeightd1 ** 2;
+const bmiJohnd1 = johnMassd1 / johnHeightd1 ** 2;
+
+const markHigherBMId1 = bmiMarkd1 > bmiJohnd1;
+
+const markMassd2 = 95;
+const johnMassd2 = 85;
+
+const markHeightd2 = 1.88;
+const johnHeightd2 = 1.76;
+
+const bmiMarkd2 = markMassd2 / markHeightd2 ** 2;
+const bmiJohnd2 = johnMassd2 / johnHeightd2 ** 2;
+
+const markHigherBMId2 = bmiMarkd2 > bmiJohnd2;
 
 console.log(
-  `I am from ${country}, which is in the continent ${continent} and its population is ${population}`
+  `For the first data set the answer is ${markHigherBMId1}. Whereas for the second data set the answer is ${markHigherBMId2}`
 );
