0424fd8b7d7d2b4644c8c6eeb8a6e748941ca148
Learning-Pythong <lamostlaaa@gmail.com>
2024-07-10 12:37:25 +1000

    Values and Variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let country = "Australia";
+let continent = "Oeania";
+
+let population = "26 Million";
+
+console.log(
+  `I am from ${country} , which is in the continent ${continent} and its population is ${population}`
+);
