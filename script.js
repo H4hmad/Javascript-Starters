1d607c14f2a6a25f01c5eec9f220c2531bfeee18
Learning-Pythong <lamostlaaa@gmail.com>
2024-07-10 13:48:34 +1000

    Lecture - 4 - let, var and const

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-let country = "Australia";
-let continent = "Oeania";
+const country = "Australia";
+const continent = "Oeania";
 
-let population = "26 Million";
+const population = 26;
+
+const isIsland = true;
+
+const language = "English";
+
+const halfCountryPopulation = population / 2;
 
 console.log(
-  `I am from ${country}, which is in the continent ${continent} and its population is ${population}`
+  `Now that the country is split in half, the population is also split in half, i-e ${halfCountryPopulation}`
 );
+
+console.log(
+  `Increasing the population of my country by 1 will give ${
+    population + 1
+  } million`
+);
+
+const populationFinland = 6;
+
+console.log(
+  `Does finland have less population than my country? i-e ${population} ? ${populationFinland}. THe answer is ${
+    populationFinland < population
+  }`
+);
+
+console.log(
+  `Does an average country with 33 million people have more population than my country?. The answer is ${
+    population < 33
+  }`
+);
+
+const description = `${country} is in ${continent}, and its ${
+  population / 2
+} million people speak ${language}`;
+
+console.log(description);
