4a7ed42104a82f361ba531268b9253b6116bfa8e
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-02 10:57:09 +1000

    Lecture 14 - If/else statements

---
+++ script.js
@@ -0,0 +1,8 @@
+const age = 19;
+
+if (age >= 18) {
+  console.log("John can obtain a license and start driving 🚘");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`John is too young. Maybe wait another ${yearsLeft} years :))`);
+}
