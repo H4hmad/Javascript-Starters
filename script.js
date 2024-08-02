6cdf3190f7382b73d71a4b5503bfa2882153e767
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-02 17:58:21 +1000

    Lecture 18 - Equality Operators

---
+++ script.js
@@ -0,0 +1,19 @@
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+
+if (numNeighbours == 1) {
+  console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+  console.log(`More than 1 border!`);
+} else if (numNeighbours == 0) {
+  console.log(`No Border at all!`);
+}
+
+if (numNeighbours === 1) {
+  console.log(`Only 1 border!`);
+} else if (numNeighbours > 1) {
+  console.log(`More than 1 border!`);
+} else if (numNeighbours === 0) {
+  console.log(`No Border at all!`);
+}
