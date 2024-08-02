62a09e1877ad908b77c6417f37ab125f5faf29fd
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-02 10:49:44 +1000

    Main Branch

--- script.js
+++
@@ -1,8 +0,0 @@
-const firstName = "Hamad";
-const job = "student";
-const birthYear = 2001;
-const year = 2024;
-
-const hamad = `I'm ${firstName}, a ${year - birthYear} years old ${job}.`;
-
-console.log(hamad);
