b11dcb3890caa43519268e93478d6508d54eb010
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-05 21:02:55 +1000

    Challenge - 4

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const mark = {
-  firstName: "Mark",
-  lastName: "Miller",
-  mass: 78,
-  height: 1.69,
-  bmi: function () {
-    return this.mass / this.height ** 2;
-  },
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+const calctip = () => {
+  for (let i = 0; i < bills.length; i++) {
+    bills[i] > 50 && bills[i] < 300
+      ? tips.push(bills[i] * 0.15)
+      : tips.push(bills[i] * 0.2);
+
+    totals.push(tips[i] + bills[i]);
+  }
+
+  console.log(`The tips are: \n ${tips}\n The totals are: \n${totals}`);
 };
 
-const john = {
-  firstName: "John",
-  lastName: "Smith",
-  mass: 92,
-  height: 1.95,
+calctip();
 
-  bmi: function () {
-    return this.mass / this.height ** 2;
-  },
+calcAverage = (arr) => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  const avg = sum / arr.length;
+  return avg;
 };
 
-console.log(
-  `${john.firstName}'s BMI (${john.bmi()}) is ${
-    john.bmi() > mark.bmi() ? "higher" : "lower"
-  } than ${mark.firstName}'s BMI (${mark.bmi()}).`
-);
+console.log(calcAverage(totals));
