204923f06c8dcf489f61d68154b550cb3186befa
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-06 12:59:59 +1000

    DOM - Project 1 - "Guess my Number!"

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+'use strict';
 
-const tips = [];
-const totals = [];
+// console.log(document.querySelector('.message').textContent);
 
-const calctip = () => {
-  for (let i = 0; i < bills.length; i++) {
-    bills[i] > 50 && bills[i] < 300
-      ? tips.push(bills[i] * 0.15)
-      : tips.push(bills[i] * 0.2);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-    totals.push(tips[i] + bills[i]);
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
+// document.querySelector('.number').textContent = '?';
+
+let score = 20;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //When there is no valid inpug
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number Input!';
+  } //When the player Wins
+  else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } //When the guess is too high
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '📈 Too High!' : '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '💥 You Lost the Game!!!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
 
-  console.log(`The tips are: \n ${tips}\n The totals are: \n${totals}`);
-};
-
-calctip();
-
-calcAverage = (arr) => {
-  let sum = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  const avg = sum / arr.length;
-  return avg;
-};
-
-console.log(calcAverage(totals));
+  document.querySelector('.number').style.width = '15rem';
+});
