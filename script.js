ccc5261f9cbdc2f257c37db3f4a361432f0a72e2
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-13 23:08:32 +1000

    Relearning some bits and pieces.

--- script.js
+++ script.js
@@ -26,4 +26,37 @@ const restaurant = {
       close: 24,
     },
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2}, and ${ing3}. ENJOYYY!`
+    );
+  },
 };
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// console.log(...restaurant.mainMenu);
+
+// const joinedArrays = [...restaurant.categories, ...restaurant.mainMenu];
+// console.log(joinedArrays);
+
+const str = 'Jonas';
+const letters = [...str, ' ', 'S.'];
+console.log(...str);
+
+const ingredients = [
+  prompt(`Let's make pasta!
+Ingredient 1?`),
+  prompt(`Ingredient 2?`),
+  prompt(`Ingredient 3?`),
+];
+
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
