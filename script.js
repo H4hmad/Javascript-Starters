e614503b8aede7efd8ca7ded47a16e156df9474c
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-17 19:42:44 +1000

    Prepping for Coding Challenge 2 (Data structures, Modern Ops and Strings)

--- script.js
+++ script.js
@@ -1,88 +1,72 @@
 'use strict';
 
-const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, // Open 24 hours
-    close: 24,
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const [players1, players2] = game.players;
 
-  //ES6 Magic
-  openingHours,
+console.log(players1, players2);
 
-  order(startingIndex, mainIndex) {
-    return [this.starterMenu[startingIndex], this.mainMenu[mainIndex]];
-  },
+const [gk, ...fieldPlayers] = players1;
+
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored!`);
 };
 
-const properties = Object.keys(openingHours);
-console.log(properties);
+printGoals('Dabies', 'Muller', 'Lewandowski', 'Kimmich');
 
-let openStr = `We are open on ${properties.length} days: `;
+printGoals('Davies', 'Muller');
+printGoals(...game.scored);
 
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-
-console.log(openStr);
-
-const values = Object.values(openingHours);
-console.log(values);
-
-const entries = Object.entries(openingHours);
-console.log(entries);
-
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open} and close at ${close}.`);
-}
-
-// if (restaurant.openingHours && restaurant.openingHours.mon) {
-//   console.log(restaurant.openingHours.mon.open);
-// }
-
-//Optional Chaining
-// console.log(restaurant.openingHours.mon?.open);
-// console.log(restaurant.openingHours?.mon?.open);
-
-// const days = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
-// for (const day of days) {
-//   console.log(day);
-//   const open = restaurant.openingHours[day]?.open ?? `closed`;
-//   console.log(`On ${day}, we open at ${open}`);
-// }
-
-// console.log(restaurant.order?.(0, 1) ?? `Method doesn't exist.`);
-
-// console.log(restaurant.orderRisotto?.(0, 1) ?? `Method doesn't exist.`);
-
-//Arrays
-
-// const users = [{ name: 'Jonas', email: 'hehe@hehe.io' }];
-// const users = [];
-
-// console.log(users[0]?.name ?? `User array empty.`);
-// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-
-// for (const item of menu) console.log(item);
-
-// for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`);
+team1 < team2 && console.log('Team 1 is more likely to win!');
+team1 > team2 && console.log('Team 2 is more likely to win!');
