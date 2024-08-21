05df5c913e4e027bd216cdabfb69a70ae6dd3996
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-21 23:07:20 +1000

    Data-Structures-Challenge-2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -40,31 +39,21 @@ const game = {
     team2: 6.5,
   },
 };
-
 const [players1, players2] = game.players;
-
 console.log(players1, players2);
-
 const [gk, ...fieldPlayers] = players1;
-
 console.log(gk, fieldPlayers);
-
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
-
 const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
-
 const {
   odds: { team1, x: draw, team2 },
 } = game;
 console.log(team1, draw, team2);
-
 const printGoals = function (...players) {
   console.log(`${players.length} goals were scored!`);
 };
-
 printGoals('Dabies', 'Muller', 'Lewandowski', 'Kimmich');
-
 printGoals('Davies', 'Muller');
 printGoals(...game.scored);
 
@@ -91,99 +80,9 @@ for (const [teamNo, teamOdds] of Object.entries(game.odds)) {
   );
 }
 
-const scoreres = {};
-
+const scorers = {};
 for (const player of game.scored) {
-  scoreres[player] ? scoreres[player]++ : (scoreres[player] = 1);
-  // console.log(scoreres[player]);
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
 }
 
-for (const [key, value] of Object.entries(scoreres)) {
-  console.log(`Key: ${key} and Value: ${value}`);
-}
-
-const orderSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
-]);
-
-console.log(orderSet);
-console.log(new Set('Jonas'));
-console.log(orderSet.size);
-console.log(orderSet.has('Pizza'));
-console.log(orderSet.has('Bread'));
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-orderSet.delete('Risotto');
-
-console.log(...orderSet);
-
-const rest = new Map();
-
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-rest.set(2, 'Lisbon, Portugal');
-
-console.log(rest.set(2, 'Lisbon, Portugal'));
-
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are Close :(');
-
-console.log(rest.get('name'));
-console.log(rest);
-
-const time = 21;
-
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
-
-console.log(rest.has('categories'));
-// rest.delete(2);
-console.log(rest);
-
-const arr = [1, 2];
-
-rest.set(arr, 'Test');
-
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.size);
-
-console.log(rest.get(arr));
-
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'Javascript'],
-  ['correct', 3],
-  [true, 'Correct'],
-  [false, 'Try again!'],
-]);
-
-console.log(question);
-
-console.log(question.get('question'));
-
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
-}
-
-// const answer = Number(prompt('Your answer'));
-
-// console.log(answer);
-
-// if (question.get('correct') === answer) {
-//   console.log(`Hooray!!! You got it right.`);
-// } else {
-//   console.log(`Try again next time.`);
-// }
-
-console.log([...question]);
+console.log(scorers);
