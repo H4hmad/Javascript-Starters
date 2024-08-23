530c14baeab2fc93f9fbb302c2398b9771423322
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-23 21:22:33 +1000

    Moving on to Part 3 of Strings.

--- script.js
+++ script.js
@@ -40,3 +40,60 @@ checkMiddleSeat('23C');
 checkMiddleSeat('3E');
 
 console.log(new String('Jonas'));
+
+console.log(airplane.toLowerCase());
+console.log(airplane.toUpperCase());
+
+const passenger = 'jOnAS';
+const passengerLower = passenger.toLowerCase();
+
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+console.log(passengerCorrect);
+
+const email = 'hello@hamad.io';
+const loginEmail = '  Hello@Hamad.Io \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!. NOW!';
+
+console.log(announcement.replaceAll('door', 'gate'));
+
+const planex = 'Airbus  A320neo';
+
+console.log(planex.includes('A320'));
+console.log(planex.includes('Boeing'));
+console.log(planex.includes('Air'));
+
+if (planex.startsWith('Airbus') && planex.endsWith('neo')) {
+  console.log('Part of the new Airbus family.');
+} else {
+  console.log(`Not part of the new airbus family. `);
+}
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log(`You are definitely not allowed on board.`);
+  } else {
+    console.log(`You are welcome on board.`);
+  }
+};
+
+checkBaggage(`I have a laptop, some Food and a pocket Knife`);
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
