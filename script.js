13d5eb7cd62ff42c8b70b77761f31f64d50c39e7
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-24 13:05:21 +1000

    Challenege 3 Complete

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 'use strict';
 
 //Challenge 3 - Data Structures
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const text = document.querySelector('textarea').value;
+console.log(text);
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  // console.log(text);
+
+  let finalText = '';
+  text.toLowerCase();
+  finalText = text.split('\n');
+  // console.log(finalText);
+  let i = 1;
+  for (let t of finalText) {
+    const [first, second] = t.toLowerCase().trim().split('_');
+    const output =
+      `${first}${second.replace(second[0], second[0].toUpperCase())}`.padEnd(
+        20,
+        ' '
+      ) + `✅`.repeat(i);
+    console.log(output);
+    // console.log(t.length);
+    i++;
+  }
+});
