03b5040436c6f4a04e0b9cb6e5ad5bbc0edb235b
Learning-Pythong <lamostlaaa@gmail.com>
2024-08-29 21:54:58 +1000

    Passing Arguments

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ createBooking('LH123', 2);
 createBooking('LH123', 2);
 
 createBooking('LH123', undefined, 2000);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedmann',
+  passport: 1234432123,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1234432123) {
+    alert(`Checked In`);
+  } else {
+    alert(`Wrong Passport!`);
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(jonas);
