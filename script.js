d51e92fdf494415875e8b265612e810a894b48c7
Learning-Pythong <lamostlaaa@gmail.com>
2024-09-05 22:27:31 +1000

    Closure - Done - Challenge - 2

--- script.js
+++ script.js
@@ -9,3 +9,11 @@
     header.style.color = `blue`;
   });
 })();
+
+//How/Why did this work?
+//The explanation here is "Closure". It works becuase of closure.
+//As can be seen. even though the envioronment in which the function
+//in the eventlistener is gone. IT is till able to access the variables
+//that were created by the time the function was created or its birth place.
+//i-e The header variable which was created in the parent function is still
+//in the backpack (Closure)
