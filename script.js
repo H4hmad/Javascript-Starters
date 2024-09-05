d27da46fdb301bef8b100aac38bae5fa458f9754
Learning-Pythong <lamostlaaa@gmail.com>
2024-09-05 22:27:15 +1000

    Closure - Done - Challenege 2

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 //Setting Up Coding Challenge 2
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  header.addEventListener(`click`, () => {
+    header.style.color = `blue`;
+  });
+})();
