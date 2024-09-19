df8548513300888e89c8872826477b598a961d1b
Learning-Pythong <lamostlaaa@gmail.com>
2024-09-19 23:47:20 +1000

    Challenege 1 - Arrays - Complete

--- script.js
+++ script.js
@@ -160,3 +160,24 @@ displayMovements(account1.movements);
 // currenciesUnique.forEach((value, key, map) => {
 //   console.log(`${key} : ${value}`);
 // });
+
+//CHALLENGE 1 - Arrays
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+
+const checkDogs = (dogsJulia, dogsKate) => {
+  const correctedArray = [...dogsJulia.slice(1, -2), ...dogsKate];
+
+  correctedArray.forEach((ele, i) => {
+    console.log(
+      `Dog number ${i + 1} is ${
+        ele >= 3 ? `an adult, and ${ele} years old.` : 'is still a puppy 🐶.'
+      }`
+    );
+  });
+};
+
+checkDogs(dogsJulia, dogsKate);
+checkDogs(dogsJulia2, dogsKate2);
