bda2121144572d2bbfa1e65649902628c80cbe08
Learning-Pythong <lamostlaaa@gmail.com>
2024-09-23 23:49:16 +1000

    Using arrow function to make code cleaner and shorter.

--- script.js
+++ script.js
@@ -186,9 +186,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
 
-const movementsUSD = movements.map(ele => {
-  return ele * eurToUsd;
-});
+const movementsUSD = movements.map(ele => ele * eurToUsd);
 
 console.log(movements);
 console.log(movementsUSD);
