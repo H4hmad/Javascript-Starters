c645082178cb075b8932ee519cbec0fabbed1d10
Learning-Pythong <lamostlaaa@gmail.com>
2024-10-02 11:00:46 +1000

    Methods Chaining Complete

--- script.js
+++ script.js
@@ -89,6 +89,20 @@ calcDisplayBalance(account1.movements);
 const calcDisplaySummary = function (mov) {
   const incomes = mov.filter(mov => mov > 0).reduce((acc, mov) => acc + mov);
   labelSumIn.textContent = `${incomes}€`;
+
+  const out = mov.filter(mov => mov < 0).reduce((acc, mov) => acc + mov);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = mov
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, mov) => acc + mov, 0);
+
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
