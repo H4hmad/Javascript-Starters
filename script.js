78d4a4363724a5e087ecc5c8a3ddd369cdaa1b78
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-03 17:06:11 +1100

    Working with Dates

--- script.js
+++ script.js
@@ -314,23 +314,53 @@ btnSort.addEventListener('click', function (e) {
 
 //Nmber Seperators
 
-const diameter = 287_460_000_000;
-console.log(diameter);
+// const diameter = 287_460_000_000;
+// console.log(diameter);
 
-const price = 345_99;
-console.log(price);
+// const price = 345_99;
+// console.log(price);
 
-const PI = 3.1415;
-console.log(PI);
+// const PI = 3.1415;
+// console.log(PI);
 
-console.log(Number('230_  000'));
+// console.log(Number('230_  000'));
 
 //BIGINT
 
-console.log(2 ** 53 - 1);
-console.log(Number.MAX_SAFE_INTEGER);
+// console.log(2 ** 53 - 1);
+// console.log(Number.MAX_SAFE_INTEGER);
 
-console.log(2348972384728947981273981273897127388n);
-console.log(BigInt(234897238472894));
-console.log(10000n + 10000n);
-console.log(239283940248340238402834n + 10000000000000n);
+// console.log(2348972384728947981273981273897127388n);
+// console.log(BigInt(234897238472894));
+// console.log(10000n + 10000n);
+// console.log(239283940248340238402834n + 10000000000000n);
+
+//CREATING DATES
+
+// const now = new Date();
+
+// console.log(now);
+
+// console.log(new Date('Aug 02 2020 18:05:41'));
+// console.log(new Date('December 24, 2015'));
+
+// console.log(new Date(account1.movementsDates[0]));
+
+//Working with Dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
