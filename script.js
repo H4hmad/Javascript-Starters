1957489fc21d5dc8d0c11df3c16e8c156bff569b
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-03 16:43:26 +1100

    The Remainder Operator

--- script.js
+++ script.js
@@ -283,21 +283,31 @@ btnSort.addEventListener('click', function (e) {
 
 //MATH and ROUNDING//
 
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
+// console.log(Math.sqrt(25));
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
 
-console.log(Math.max(5, 2, 4, 6, 7, 12, 23, 4, 34));
-console.log(Math.min(5, 2, 4, 6, 7, 12, 23, 4, 34));
+// console.log(Math.max(5, 2, 4, 6, 7, 12, 23, 4, 34));
+// console.log(Math.min(5, 2, 4, 6, 7, 12, 23, 4, 34));
 
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-console.log(Math.trunc(Math.random() * 6) + 1);
+// console.log(Math.trunc(Math.random() * 6) + 1);
 
-const randomInt = (min, max) =>
-  Math.trunc(Math.random() * (max - min) + 1) + min;
+// const randomInt = (min, max) =>
+//   Math.trunc(Math.random() * (max - min) + 1) + min;
 
-console.log(randomInt(10, 20));
+// console.log(randomInt(10, 20));
 
-//Rounding Integers
-console.log(Math.trunc(23.3));
+// //Rounding Integers
+// console.log(Math.trunc(23.3));
+
+//Remainder Operator
+
+labelBalance.addEventListener('click', () => {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
