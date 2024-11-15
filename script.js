2fa54ac6d8829fcc071365f9298ce994ee1ac032
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-15 22:48:57 +1100

    Styles, Attributes and Classes

--- script.js
+++ script.js
@@ -71,3 +71,32 @@ document.querySelector('.btn--close-cookie').addEventListener('click', () => {
 //STYLES
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
+
+// console.log(message.style.color);
+
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+
+logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+
+logo.setAttribute('company', 'Bankist');
+console.log(logo.getAttribute('src'));
+
+//Data Attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes
+logo.classList.add('c');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
