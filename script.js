d682c3e88a6d9b5750676785fdd2aac83ef8d3f2
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-23 15:04:23 +1100

    Smooth Scrolling Complete - For real this time.

--- script.js
+++ script.js
@@ -107,24 +107,23 @@ const section1 = document.querySelector('#section--1');
 
 btnScrollTo.addEventListener('click', e => {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+  // console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+  // console.log(e.target.getBoundingClientRect());
 
-  console.log(`Current scroll (X/Y): `, window.scrollX, scrollY);
+  // console.log(`Current scroll (X/Y): `, window.scrollX, scrollY);
 
-  console.log(
-    'Height/width viewport: ',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+  // console.log(
+  //   'Height/width viewport: ',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
 
-  // window.scrollTo(s1coords.left + window.scrollX, s1coords.top + scrollY);
-  // window.scrollTo({
-  //   left: s1coords.left + window.scrollX,
-  //   top: s1coords.top + scrollY,
-  //   behavior: 'smooth',
-  // });
+  window.scrollTo({
+    left: s1coords.left + window.scrollX,
+    top: s1coords.top + window.scrollY,
+    behavior: 'smooth',
+  });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
+  // section1.scrollIntoView({ behavior: 'smooth' });
 });
