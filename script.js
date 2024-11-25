eebb4b2adb007522279a41fadd437f6a2bb0a7f7
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-25 23:47:13 +1100

    Event Propagation in Action

--- script.js
+++ script.js
@@ -146,3 +146,17 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = e => {
 //   alert('addEventListener: Great the mouseenter event has now been triggered.');
 // };
+
+//rgb(255,255,255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav__links').addEventListener('click', e => {});
+document.querySelector('.nav').addEventListener('click', e => {});
