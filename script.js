60f9c08aed787cf902f4c0d233c135e873cc72db
Learning-Pythong <lamostlaaa@gmail.com>
2024-11-28 16:22:50 +1100

    Event Propagation Complete

--- script.js
+++ script.js
@@ -156,11 +156,18 @@ console.log(randomColor(0, 255));
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  //Stop Propagation
+  // e.stopPropagation();
 });
 
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
 });
 document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
 });
